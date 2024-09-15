1ea34066897c21b475c6f9fab0d42d842be2aaf3
AugustoCVS <guty.vertolis@gmail.com>
2024-09-15 16:20:21 -0300

    feat: add login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
-import { RegisterComponent } from './core/auth/pages/register/register.component';
+import { RegisterComponent } from './auth/pages/register/register.component';
+import { LoginComponent } from './auth/pages/login/login.component';
 
 export const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path: '',
+    component: LoginComponent
   }
 ];
