e80dcaea5d4a3f575522a7b3cf1a3f270175880f
AugustoCVS <guty.vertolis@gmail.com>
2024-09-15 16:23:08 -0300

    feat: create register component

---
+++ src/app/auth/pages/register/register.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../core/service/auth/auth.service';
+import { CommonModule } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+  loading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  isLoading$ = this.loading.asObservable();
+  errorMessage: string = '';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) { }
+
+  passwordMatchValidator(formGroup: FormGroup) {
+    const password = formGroup.get('password')?.value;
+    const confirm_password = formGroup.get('confirm_password')?.value;
+    return password === confirm_password ? null : { passwordMismatch: true };
+  }
+
+  initializeForm() {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirm_password: ['', Validators.required]
+    }, { validators: this.passwordMatchValidator });
+  }
+
+  submit() {
+    if (this.form.valid) {
+      this.loading.next(true)
+      this.authService.register(this.form.getRawValue())
+        .subscribe({
+          next: () => {
+            this.loading.next(false)
+          },
+          error: () => {
+            this.errorMessage = 'Registration failed. Please try again.';
+            this.loading.next(false)
+          }
+        });
+      return;
+    }
+
+    return this.form.markAllAsTouched()
+  }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+
+}
