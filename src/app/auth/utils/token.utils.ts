9b7e40a2409caccfa4caf5b9ff7e645417ac0b3d
AugustoCVS <guty.vertolis@gmail.com>
2024-09-15 22:15:39 -0300

    refactor: adjust token utils

--- src/app/auth/utils/token.utils.ts
+++ src/app/auth/utils/token.utils.ts
@@ -4,24 +4,20 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class TokenUtils {
-  token: string = '';
-  refreshToken: string = '';
 
   handleSaveTokenOnStorage({ token, refreshToken }: { token: string; refreshToken: string }) {
-    this.token = token;
-    this.refreshToken = refreshToken;
     localStorage.setItem('token', token);
     localStorage.setItem('refreshToken', refreshToken);
   }
 
   handleGetTokenFromStorage() {
-    this.token = localStorage.getItem('token') || '';
-    this.refreshToken = localStorage.getItem('refreshToken') || '';
+    return {
+      token: localStorage.getItem('token') || '',
+      refreshToken: localStorage.getItem('refreshToken') || ''
+    };
   }
 
   handleRemoveTokenFromStorage() {
-    this.token = '';
-    this.refreshToken = '';
     localStorage.removeItem('token');
     localStorage.removeItem('refreshToken');
   }
