2e9b295e4b5669bef23334e6f00ca4f3ba936acd
AugustoCVS <guty.vertolis@gmail.com>
2024-09-16 21:02:16 -0300

    feat: add props on LoadUserInfo

--- src/app/store/user/user.actions.ts
+++ src/app/store/user/user.actions.ts
@@ -1,7 +1,7 @@
 import { createAction, props } from "@ngrx/store";
 import { IUserResponse } from "../../core/service/user/interfaces/user";
 
-const loadUserInfo = createAction('[User] Load user info')
+const loadUserInfo = createAction('[User] Load user info', props<{ userId: string }>())
 const loadUserInfoSuccess = createAction('[User] Load user info success', props<{ user: IUserResponse }>())
 
 export const userActions = {
