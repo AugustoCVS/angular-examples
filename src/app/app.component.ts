517439535e8a3973f09f8474fa64a692f1b113d0
AugustoCVS <guty.vertolis@gmail.com>
2024-09-16 23:18:52 -0300

    refactor: call loadUserInfo on AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { TokenUtils } from './auth/utils/token.utils';
+import { userActions } from './store/user/user.actions';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +11,21 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  store = inject(Store)
+  sub: string = '';
+
+  constructor(
+    private tokenUtils: TokenUtils
+  ) {
+    this.sub = this.tokenUtils.handleDecodeToken()?.sub || ''
+  }
+
+  ngOnInit(): void {
+    if (this.sub) {
+      this.store.dispatch(userActions.loadUserInfo({ userId: this.sub }))
+    }
+  }
+
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,7 @@
-import { Component, inject, OnInit } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs";
-import { userState } from "../../store/user/user.reducer";
 import { userSelector } from "../../store/user/user.selector";
-import { userActions } from "../../store/user/user.actions";
-import { TokenUtils } from "../../auth/utils/token.utils";
 import { IUserResponse } from "../../core/service/user/interfaces/user";
 import { CommonModule } from "@angular/common";
 
@@ -15,22 +12,8 @@ import { CommonModule } from "@angular/common";
   standalone: true,
   imports: [CommonModule]
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent {
   store = inject(Store)
-  sub: string = '';
-
-  constructor(
-    private tokenUtils: TokenUtils
-  ) {
-    this.sub = this.tokenUtils.handleDecodeToken().sub || ''
-  }
-
   user$: Observable<IUserResponse> = this.store.select(userSelector)
-
-  ngOnInit(): void {
-    this.store.dispatch(userActions.loadUserInfo({ userId: this.sub }))
-  }
-
 }
 
