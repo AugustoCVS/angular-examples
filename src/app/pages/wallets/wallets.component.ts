d063c07234df140466cedfc2ac4513aa917ff8bb
AugustoCVS <guty.vertolis@gmail.com>
2024-10-01 21:29:16 -0300

    feat: signals with rxjs

--- src/app/pages/wallets/wallets.component.ts
+++ src/app/pages/wallets/wallets.component.ts
@@ -1,18 +1,31 @@
-import { JsonPipe } from '@angular/common';
-import { Component, computed, effect, signal } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { Component, computed, effect, inject, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+
+import { OnPushTesteComponent } from '../../components/onpush/onpush.component';
+import { SignalExemploService } from '../../core/service/signals/signal-exemplo.service';
 
 @Component({
   selector: 'app-wallets',
   standalone: true,
-  imports: [JsonPipe],
+  imports: [JsonPipe, OnPushTesteComponent, AsyncPipe],
   templateUrl: './wallets.component.html',
   styleUrl: './wallets.component.scss'
 })
 export class WalletsComponent {
 
+  private signalService = inject(SignalExemploService)
   protected exemploCount = signal(1)
-
   protected showCount = signal(true)
+  protected exemploCount$ = toObservable(this.exemploCount)
+  protected exemploObservableToSignal = toSignal(this.exemploCount$)
+
+  constructor() {
+    effect(() => {
+      console.log(`ALTEROU NO EFFECT ${this.exemploCount()}`)
+    })
+  }
+
   protected computedExemplo = computed(() => {
     console.log('computed acionado')
 
@@ -23,18 +36,14 @@ export class WalletsComponent {
     }
   })
 
+  meuObs$ = this.signalService.getCount()
+
   executar() {
     this.exemploCount.update(atual => atual + 1)
   }
 
   toggleShowCount() {
     this.showCount.update(val => !val)
-  }
-
-  constructor() {
-    effect(() => {
-      console.log(`ALTEROU NO EFFECT ${this.exemploCount()}`)
-    })
   }
 
 }
